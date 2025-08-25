6b9998bbce970a5424e1a35a7303a1dd274b4505
Kan-à-Pé <alexandre.mandon0@gmail.com>
2025-08-25 11:46:20 +0200

    feat: ajout des routes pour la gestion des notes

--- secure-notes-backend/index.js
+++ secure-notes-backend/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import authRoutes from './routes/auth.js'
+import notesRoutes from './routes/notes.js'
 
 dotenv.config()
 
@@ -10,6 +11,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api', authRoutes)
+app.use('/api/notes', notesRoutes)
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
